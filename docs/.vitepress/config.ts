9aab2c080a5f1f1d8764e87ada9b816aca63f696
Volodymyr Baranchuk <volodymyr.baranchuk@teladochealth.com>
2023-06-07 15:10:31 +0300

    test

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Flow UI Web Core docs",
   description: "A VitePress Site",
-  base: "/monorepo-vitepress/",
+  base: "/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
